ef2d4f6149031d852dfab74d83533385afaf98ef
efreneau <efreneau@users.noreply.github.com>
2019-08-18 19:14:33 -0700

    Added code for layer 2 comms
    Added
    -COBS
    -CRC-32
    -short test of both
    
    Both are written with fast integers and the crc code uses a lookup table for additional speed.

---
+++ Layer2/COBS.c
@@ -0,0 +1,101 @@
+#include <inttypes.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#include <stdlib.h>
+#include <time.h>
+
+#define StartBlock()	(code_ptr = dst++, code = 1)
+#define FinishBlock()	(*code_ptr = code)
+
+size_t StuffData(uint8_t *ptr, size_t length, uint8_t *dst)
+{
+	const uint8_t *start = dst, *end = ptr + length;
+	uint8_t code, *code_ptr; // Where to insert the leading count
+	StartBlock();
+	
+	while (ptr < end) {//loop over data
+		if (code != 0xFF) {
+			uint8_t c = *ptr++;
+			if (c != 0) {
+				*dst++ = c;
+				code++;
+				continue;
+			}
+		}
+		FinishBlock();
+		StartBlock();
+	}
+	FinishBlock();
+	return dst - start;
+}
+
+size_t UnStuffData(uint_fast8_t *ptr, size_t length, uint_fast8_t *dst)
+{
+	const uint_fast8_t *start = dst, *end = ptr + length;
+	uint_fast8_t code = 0xFF, copy = 0;
+
+	for (; ptr < end; copy--) {
+		if (copy != 0) {
+			*dst++ = *ptr++;
+		} else {
+			if (code != 0xFF)
+				*dst++ = 0;
+			copy = code = *ptr++;
+			if (code == 0)
+				break;
+		}
+	}
+	return dst - start;
+}
+
+int main()
+{
+	const int frame_length = 254; 
+	char s[frame_length];
+	uint_fast8_t s_byte[frame_length];
+	uint_fast8_t out[frame_length+1];
+	uint_fast8_t test[frame_length];
+	
+	//Generate
+	
+	int i;
+	srand(time(NULL));
+	for (i=0;i<frame_length;i++){
+		//s[i]=(rand()%26)+65;//letter space
+		//s[i]=rand() % 256;//arbitrary
+		s[i] = 0;//reserved byte mine field
+	}
+	//s[0] = 0xff;//testing altered first bit
+	
+	//printf("string: \t%s\n",s);
+	printf("Random Byte Frame (%d Bytes).\n",frame_length);
+	
+	//to byte array.
+	for (i=0;i<frame_length;i++)
+		s_byte[i]= (uint8_t) s[i];
+	
+	//COBS
+	double time_spent = 0.0;
+	clock_t begin = clock();
+	size_t length = StuffData(s_byte, (size_t) frame_length, out);
+	clock_t end = clock();
+	
+	time_spent = (double)(end - begin)*1000000 / CLOCKS_PER_SEC;
+
+	printf("Time elpased is %d microseconds.\n", (int)time_spent);
+	printf("Total Size: %lu Bytes (+%lu).",length,length-frame_length);
+	printf("\n=================================\n");
+	
+	size_t length2 = UnStuffData(out, (size_t) frame_length+1, test);
+	
+	for (int i = 0; i <= 255; i++){
+		//printf("0x%02x|", s_byte[i]);//data
+		//printf("0x%02x|", out[i]);//stuffed
+		//printf("0x%02x ", test[i]);//stuffed then unstuffed
+		printf("%s ", (s_byte[i] == test[i]) ? "true" : "false");//stuffed then unstuffed check
+		if (i % 8 == 7)
+			printf("\n");
+	}
+	return 0;
+}
